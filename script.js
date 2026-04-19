5c2bf6a41da8bc627aaa1eaf1b17c3bebbb725c3
risho2 <ryo.otk22@gmail.com>
2026-04-19 15:35:32 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ window.onload = function() {
     document.getElementById('consentButton').addEventListener('click', function() {
         // GoogleフォームのURLリスト
         var forms = [
-            "https://docs.google.com/forms/d/e/1FAIpQLSfvgKdkpbo5S2midiFM2IWQmLp_NMOxMDpUTaIWq8sc9FnJEA/viewform?usp=header",
-            "https://docs.google.com/forms/d/e/1FAIpQLSe6oykAyBTFuOgrpLdO1URF6IAQZ2RSbrhzQXEA0IUBuesuZQ/viewform?usp=header",
-            "https://docs.google.com/forms/d/e/1FAIpQLSfABvu8eE1ZU06gHe9q2ApN5fS5dhut8UV7kL-eeTJ0b_pfzQ/viewform?usp=header",
-            "https://docs.google.com/forms/d/e/1FAIpQLSdU3XdV5mQDZVirqjupO6MGMj1CJ0j25phUWrB8BJclqJFT7w/viewform?usp=header",
-            "https://docs.google.com/forms/d/e/1FAIpQLSfuw1urbqdQZeqLiJTf8YinQ5yxSlIh3am9c3cAdEcVjRTEHw/viewform?usp=header",
-            "https://docs.google.com/forms/d/e/1FAIpQLSeACM6YVASGPMu5JLLiu3BVqXhIZGey0idLFAQJIodlk77SDA/viewform?usp=header",
-            "https://docs.google.com/forms/d/e/1FAIpQLSdRslhTRapdJrpY_45XvymIN75TAfhc3G0NJ0pj5Afslonj6A/viewform?usp=header",
-            "https://docs.google.com/forms/d/e/1FAIpQLSfVsdsRLRwGTHEtAi9BChYBYm4iAxdwmFgBrtVZJuSvm4YAgQ/viewform?usp=header"
+            "https://docs.google.com/forms/d/e/1FAIpQLSfF1lCqtsCnbt3iiQzNXP9TEwg6xF1eker_JaGFypE_nrLxtw/viewform?usp=publish-editor",
+            "https://docs.google.com/forms/d/e/1FAIpQLSfNBjHbdRk6kizDguUsXs24ruiD5VyGpaM5j8GxjkcycYtChg/viewform?usp=publish-editor",
+            "https://docs.google.com/forms/d/e/1FAIpQLSewyk-nHXR6bFpbBKU3t0Mb2NZBmmGVk0o26zE0WWZ-T3SiKA/viewform?usp=publish-editor",
+            "https://docs.google.com/forms/d/e/1FAIpQLSdBfHqLZ5szpzgY-eF7IjMR0qFtkevHdd05_zbAJKIZqbpcPA/viewform?usp=publish-editor",
+            "https://docs.google.com/forms/d/e/1FAIpQLSfVIgupvre9HVeHlm1gpNjxlIBBEnwUkbi2g7_iMRWZJGtPrA/viewform?usp=publish-editor",
+            "https://docs.google.com/forms/d/e/1FAIpQLSfdTJFNh5Ibp6ddY88MaSg6fpMlzgPxhAyX3fTSyqitRdRMuA/viewform?usp=publish-editor",
+            "https://docs.google.com/forms/d/e/1FAIpQLSdpeKGBFJXDFYdKyg2sjAGu-cTzQsJTAopfifhsxukOws24aA/viewform?usp=publish-editor",
+            "https://docs.google.com/forms/d/e/1FAIpQLSfz64YlZZpcROim9OMdnEikRLMLqzClfsmi5PfukThCFs3PfA/viewform?usp=publish-editor"
         ];
 
         // ランダムなURLを選択
